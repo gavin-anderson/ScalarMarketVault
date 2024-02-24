410ff1005b29624cb3f4b630e47b6ca5fc4522cf
Gavin Anderson <anderson.gavink@gmail.com>
2024-02-23 20:24:04 -0500

    Addition of UniswapV3Pool and No Delegate Call, Vault logic altered and naming conventions changed

---
+++ contracts/ScalarMarketVault.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import './LongToken.sol';
+import './ShortToken.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ScalarMarketVault  {
+    IERC20 public usdc;
+    LongToken public longToken;
+    ShortToken public shortToken;
+
+    uint256 public BASE = 1000;
+    uint256 public longPrice;
+
+    uint256 public startRange;
+    uint256 public endRange;
+    uint256 public currentValue;
+
+    constructor(address _usdcAddress, address _longTokenAddress, address _shortTokenAddress, uint256 _startRange, uint256 _endRange, uint256 _currentValue)  {
+        usdc = IERC20(_usdcAddress);
+        longToken = LongToken(_longTokenAddress);
+        shortToken = ShortToken(_shortTokenAddress);
+        startRange = _startRange;
+        endRange = _endRange;
+        currentValue = _currentValue;
+    }
+
+    function mintLongShort(uint256 usdcAmount) public {
+        require(usdc.transferFrom(msg.sender, address(this), usdcAmount), "USDC transfer failed");
+
+    
+        longToken.mint(msg.sender, usdcAmount); 
+        shortToken.mint(msg.sender, usdcAmount); 
+    }
+
+    function redeem(uint256 longAmount, uint256 shortAmount) public {
+        require(longToken.transferFrom(msg.sender, address(this), longAmount), "Long token transfer failed");
+        require(shortToken.transferFrom(msg.sender, address(this), shortAmount), "Short token transfer failed");
+
+        getLongPrice();
+        uint256 shortPrice = BASE - longPrice; 
+        
+        uint256 totalValue = (longAmount * longPrice + shortAmount * shortPrice) / BASE;
+        require(usdc.transfer(msg.sender, totalValue), "USDC transfer failed");
+    }
+
+    function getLongPrice()public{
+            uint256 distanceFromStart = currentValue - startRange;
+            uint256 rangeLength = endRange - startRange;
+            uint256 tempPrice = distanceFromStart*1000/rangeLength;
+            longPrice = min(1,tempPrice);
+    }
+
+
+    function setCurrentValue(uint256 _currentValue)external {
+            currentValue = _currentValue;
+    }
+
+    function min(uint256 a, uint256 b)internal returns(uint256){
+        if (a<b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+}
